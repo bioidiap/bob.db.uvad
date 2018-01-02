2248185042a9dedd75addf7c22f3bc7b5b350d0d
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-01-02 13:42:19 +0100

    Add bob.bio.config entry point

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, dist
-dist.Distribution(dict(setup_requires = ['bob.extension']))
+dist.Distribution(dict(setup_requires=['bob.extension']))
 
 # load the requirements.txt for additional requirements
 from bob.extension.utils import load_requirements, find_packages
@@ -13,31 +13,31 @@ setup(
 
     # This is the basic information about your project. Modify all this
     # information before releasing code publicly.
-    name = 'bob.db.uvad',
-    version = open("version.txt").read().rstrip(),
-    description = 'UVAD Database Access in Bob',
+    name='bob.db.uvad',
+    version=open("version.txt").read().rstrip(),
+    description='UVAD Database Access in Bob',
 
-    url = 'https://gitlab.idiap.ch/bob/bob.db.uvad',
-    license = 'GPLv3',
-    author = 'Amir Mohammadi',
-    author_email = 'amir.mohammadi@idiap.ch',
-    keywords = 'bob',
+    url='https://gitlab.idiap.ch/bob/bob.db.uvad',
+    license='GPLv3',
+    author='Amir Mohammadi',
+    author_email='amir.mohammadi@idiap.ch',
+    keywords='bob',
 
     # If you have a better, long description of your package, place it on the
     # 'doc' directory and then hook it here
-    long_description = open('README.rst').read(),
+    long_description=open('README.rst').read(),
 
     # This line is required for any distutils based packaging.
     # It will find all package-data inside the 'bob' directory.
-    packages = find_packages('bob'),
-    include_package_data = True,
+    packages=find_packages('bob'),
+    include_package_data=True,
 
     # This line defines which packages should be installed when you "install"
     # this package. All packages that are mentioned here, but are not installed
     # on the current system will be installed locally and only visible to the
     # scripts of this package. Don't worry - You won't need administrative
     # privileges when using buildout.
-    install_requires = install_requires,
+    install_requires=install_requires,
 
     # This entry defines which scripts you will have inside the 'bin' directory
     # once you install the package (or run 'bin/buildout'). The order of each
@@ -53,27 +53,31 @@ setup(
     #
     # In this simple example we will create a single program that will print
     # the version of bob.
-    entry_points = {
+    entry_points={
 
-      'bob.db' : [
-        'uvad = bob.db.uvad.driver:Interface',
-      ],
+        'bob.db': [
+            'uvad = bob.db.uvad.driver:Interface',
+        ],
 
-      'bob.pad.database': [
-        'uvad = bob.db.uvad.config:database',
-      ],
+        'bob.pad.database': [
+            'uvad = bob.db.uvad.config:database',
+        ],
+
+        'bob.bio.config': [
+            'uvad = bob.db.uvad.config:database',
+        ],
     },
 
     # Classifiers are important if you plan to distribute this package through
     # PyPI. You can find the complete list of classifiers that are valid and
     # useful here (http://pypi.python.org/pypi?%3Aaction=list_classifiers).
-    classifiers = [
-      'Framework :: Bob',
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-      'Natural Language :: English',
-      'Programming Language :: Python',
-      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    classifiers=[
+        'Framework :: Bob',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
